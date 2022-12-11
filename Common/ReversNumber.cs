c6842c501b3aed98d56d3ad282a355001cc42af6
AndreyRomanuk <krark1986@github.com>
2022-12-11 14:30:01 +0300

    add task 19 and library Common

---
+++ Common/ReversNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace Common;
+
+public static class ReversNumber
+{
+    public static int GetNumberFromUser()
+    {
+        bool isParsed = int.TryParse(Console.ReadLine(),out int number);
+
+        if(isParsed)
+        {
+            return number;
+        }
+        else{
+            return -1;
+        }
+    }
+    public static int GetNumberOfRevers(int num)
+    {
+        int resNum = 0;
+
+        while (num > 0)
+        {
+            resNum = resNum * 10 + num % 10;
+            num = num / 10;
+        }
+        return resNum;
+    }
+}
---
+++ HomeWork3/task19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+using Common;
+
+Console.WriteLine("Введите число");
+
+int number = int.Parse(Console.ReadLine());
+
+int revNum = ReversNumber.GetNumberOfRevers(number);
+
+Console.WriteLine(revNum);
+
+if (revNum == number)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
