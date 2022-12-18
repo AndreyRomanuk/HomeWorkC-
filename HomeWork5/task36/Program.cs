36dad735d47987b7e012922fddbd895394503f69
AndreyRomanuk <krark1986@github.com>
2022-12-18 16:51:47 +0300

    add task 36

---
+++ HomeWork5/task36/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+using Common;
+
+int[] array = CreatRandomArray.CreatNewRandomArray(8, 0, 100);
+
+PrintArray(array);
+
+int result = SumUnevenNambers(array);
+
+Console.WriteLine(result);
+
+
+
+
+int SumUnevenNambers(int[] array)
+{
+    int result = 0;
+    for (int i = 1; i < array.Length; i += 2)
+    {
+        result += array[i];
+    }
+    return result;
+}
+
+void PrintArray(int[] array)
+{
+    string str = string.Join(", ", array);
+    Console.WriteLine($"[ {str} ]");
+}
