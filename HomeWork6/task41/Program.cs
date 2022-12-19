cdf799ac4f379e7d99c1c05cbebdeabe012255d7
AndreyRomanuk <krark1986@github.com>
2022-12-19 21:26:13 +0300

    add task 41

---
+++ HomeWork6/task41/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] array = CreatNewRandomArray(5);
+
+PrintArray(array);
+
+int result = PositiveNumbers(array);
+
+Console.WriteLine(result);
+
+int PositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+int[] CreatNewRandomArray(int size)
+{
+    int[] array = new int[size];
+
+    for (var i = 0; i < size; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    string str = string.Join(", ", array);
+    Console.WriteLine($"[ {str} ]");
+}
