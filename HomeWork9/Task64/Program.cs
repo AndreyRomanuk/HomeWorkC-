a9917cde36afe9f4dd7fb5aab1329a9cbee31374
AndreyRomanuk <krark1986@github.com>
2023-01-11 20:05:40 +0300

    Add Task 64 to HomeWork 9

---
+++ HomeWork9/Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using Common;
+
+int number  = InputData.IntData("Введите число:");
+DecreaseNumbers(number);
+
+void DecreaseNumbers(int number, int tmp = 1)
+{
+    Console.Write($"{number}, ");
+    number--;
+    if(tmp > number)
+    {
+        return;
+    }
+    DecreaseNumbers(number, tmp);
+}
