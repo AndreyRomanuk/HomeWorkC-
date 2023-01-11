b708d78fe8685ca6aa3038d269dd52becfc11433
AndreyRomanuk <krark1986@github.com>
2023-01-11 20:10:06 +0300

    Add Task 68 to HomeWork 9

---
+++ HomeWork9/Task68/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+using Common;
+
+int numberM = InputData.IntData("Введите число M:");
+int numberN = InputData.IntData("Введите число N:");
+Console.WriteLine($"A({numberM}, {numberN}) = {Akkerman(numberM, numberN)}");
+
+int Akkerman(int n, int m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return Akkerman(n - 1, 1);
+    else
+      return Akkerman(n - 1, Akkerman(n, m - 1));
+}
